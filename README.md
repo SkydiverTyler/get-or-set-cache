0bf86bfa286ebb378463bde6ff7259856190076d
Tyler <skydivertyler@gmail.com>
2024-08-16 22:54:25 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # get-or-set-cache
- 
+
+## Args
+
+1. `key` Cache key
+
+-   Can be any string
+
+2. `ttl` Max cache time
+
+-   In seconds
+-   Set this to `false` to debug the callback function and prevent caching
+
+3. Callback function
+
+-   Async
+-   The return value will be cached
+
+## Returns
+
+A promise.
+The callback function's return value.
+If the value has already been cached, it will return that.
+If the value has not been cached, it will run and await the callback function, set the cache, and return the value.
---
+++ index.js
@@ -0,0 +1,36 @@
+import NodeCache from "node-cache";
+
+const DEFAULT_TTL = 60 * 60 * 24;
+
+const CACHE = new NodeCache({
+    stdTTL: DEFAULT_TTL,
+    checkperiod: 0,
+});
+
+export const getOrSetCache = async (key, ttl, fn) => {
+    // Set TTL to `false`
+    // to debug the callback function (prevent caching)
+    if (ttl === false) {
+        // Also clear stale value
+        CACHE.del(key);
+        return fn();
+    }
+
+    // Check to see if value has already been cached
+    const val = CACHE.get(key);
+
+    if (val) {
+        // Cache exists, return it
+        return val;
+    } else {
+        // Cache doesn't exist, run callback and store value
+        const newVal = await fn();
+
+        // If TTL param is nullish, set to the default
+        if (newVal != null) {
+            CACHE.set(key, newVal, ttl ?? DEFAULT_TTL);
+        }
+
+        return newVal;
+    }
+};
